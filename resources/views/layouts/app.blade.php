0ed715469f5d60455565fe359da5d8dad0d1dafd
Jose Oviedo <guitaroviedo46@gmail.com>
2021-03-31 10:15:51 -0500

    Variables para fuentes y colores personzalizados

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,35 +6,30 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         {{-- Favicon --}}
-        <link rel="apple-touch-icon" sizes="57x57" href="{{asset('images/apple-icon-57x57.png') }}">
-        <link rel="apple-touch-icon" sizes="60x60" href="{{asset('images/apple-icon-60x60.png') }}">
-        <link rel="apple-touch-icon" sizes="72x72" href="{{asset('images/apple-icon-72x72.png') }}">
-        <link rel="apple-touch-icon" sizes="76x76" href="{{asset('images/apple-icon-76x76.png') }}">
-        <link rel="apple-touch-icon" sizes="114x114" href="{{asset('images/apple-icon-114x114.png') }}">
-        <link rel="apple-touch-icon" sizes="120x120" href="{{asset('images/apple-icon-120x120.png') }}">
-        <link rel="apple-touch-icon" sizes="144x144" href="{{asset('images/apple-icon-144x144.png') }}">
-        <link rel="apple-touch-icon" sizes="152x152" href="{{asset('images/apple-icon-152x152.png') }}">
-        <link rel="apple-touch-icon" sizes="180x180" href="{{asset('images/apple-icon-180x180.png') }}">
-        <link rel="icon" type="image/png" sizes="192x192"  href="{{asset('images/android-icon-192x192.png') }}">
-        <link rel="icon" type="image/png" sizes="32x32" href="{{asset('images/favicon-32x32.png') }}">
-        <link rel="icon" type="image/png" sizes="96x96" href="{{asset('images/favicon-96x96.png') }}">
-        <link rel="icon" type="image/png" sizes="16x16" href="{{asset('images/favicon-16x16.png') }}">
-        <link rel="manifest" href="{{asset('images/manifest.json') }}">
+        <link rel="apple-touch-icon" sizes="57x57" href="{{asset('images/favicon/apple-icon-57x57.png') }}">
+        <link rel="apple-touch-icon" sizes="60x60" href="{{asset('images/favicon/apple-icon-60x60.png') }}">
+        <link rel="apple-touch-icon" sizes="72x72" href="{{asset('images/favicon/apple-icon-72x72.png') }}">
+        <link rel="apple-touch-icon" sizes="76x76" href="{{asset('images/favicon/apple-icon-76x76.png') }}">
+        <link rel="apple-touch-icon" sizes="114x114" href="{{asset('images/favicon/apple-icon-114x114.png') }}">
+        <link rel="apple-touch-icon" sizes="120x120" href="{{asset('images/favicon/apple-icon-120x120.png') }}">
+        <link rel="apple-touch-icon" sizes="144x144" href="{{asset('images/favicon/apple-icon-144x144.png') }}">
+        <link rel="apple-touch-icon" sizes="152x152" href="{{asset('images/favicon/apple-icon-152x152.png') }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{asset('images/favicon/apple-icon-180x180.png') }}">
+        <link rel="icon" type="image/png" sizes="192x192"  href="{{asset('images/favicon/android-icon-192x192.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{asset('images/favicon/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="96x96" href="{{asset('images/favicon/favicon-96x96.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{asset('images/favicon/favicon-16x16.png') }}">
+        <link rel="manifest" href="{{asset('images/favicon/manifest.json') }}">
         <meta name="msapplication-TileColor" content="#ffffff">
-        <meta name="msapplication-TileImage" content="{{asset('images/ms-icon-144x144.png') }}"">
+        <meta name="msapplication-TileImage" content="{{asset('images/favicon/ms-icon-144x144.png') }}"">
         <meta name="theme-color" content="#ffffff">
 
         <title>{{ __('App name') }}</title>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lexend:wght@400;600;700&display=swap">
-
-        <!-- Styles -->
+        <!-- Styles y Bootstrap compilado-->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
-
-        <!-- Bootstrap 5 -->
-        <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+        
         <!-- Fontawesome -->
         <link rel="stylesheet" href="{{ asset('fontawesome/css/all.min.css') }}">
         
@@ -54,7 +49,7 @@
                         </div>
                     </div>
                     @yield('content')
-                    
+                    Ww
                 </main>
             </div>
         </div>
