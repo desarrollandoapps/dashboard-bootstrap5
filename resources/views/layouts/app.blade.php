452a8474171fe8a96de4fef88abca121cec2ea7b
Jose Oviedo <guitaroviedo46@gmail.com>
2021-04-03 16:57:39 -0500

    Add bootstrap.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,7 +56,7 @@
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
-        <script src="{{ asset('bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
         <script src="{{ asset('fontawesome/js/all.min.css') }}"></script>
     </body>
 </html>
